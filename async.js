4e25a5db291932fc9c47bde908922a2b3d03440e
Manoj-541 <152593959+Manoj-541@users.noreply.github.com>
2025-01-24 13:01:37 +0530

    Add files via upload

--- async.js
+++ async.js
@@ -66,25 +66,37 @@
 // }
 // fetchUsers();
 
-
+// ! async and await
+// function demo()
+// {
+// console.log("Start");
+// console.log(10);
+// console.log(20);
+// return;
+// console.log(30);
+// console.log(40);
+// console.log("End");
+// }
+// demo();
 
 let p = new Promise((resolve , reject)=>{
     setTimeout(()=>{
         resolve("success");
     }, 5000)
 })
+
 async function demo(){
     console.log("Start");
-    let x = await p;
+    let x =await p;
     console.log(x);
     console.log("End");
 };
-demo();
+demo()
 
-async function fetchUsers() {
-    let response = await fetch("https://jsonplaceholder.typicode.com/users");
-    let data = await response.json();
+async function fetchUsers()
+{
+    let response =await fetch("https://jsonplaceholder.typicode.com/users");
+    let data =await response.json();
     console.log(data);
-    
 }
 fetchUsers();
---
+++ dom.js
@@ -0,0 +1,30 @@
+// // console.log(window);
+// // console.log(document);
+
+// // let element = document.getElementById("demo");
+// // element.innerText = "G+"
+// // console.log(element);
+
+// let ele = document.getElementById("test");
+// // ele.innerText= "<h1>Header</h1>"
+// ele.innerHTML = "<h1>Header</h1>"
+// console.log(ele);
+
+// //! 
+// let ele = document.getElementsByClassName("test");
+// console.log(ele);
+// console.log(Array.isArray(ele));
+// ele[0].innerHTML = "Hello"
+// // ele[0].style.backgroundColor = "tomato"
+// //! 
+// let x= [...ele];
+// console.log(x, Array.isArray(x));
+// x.map(element =>{
+//     element.style.backgroundColor = "orange"
+// })
+// let ele = document.querySelectorAll("#mru");
+// console.log(ele);
+let ele = document.querySelectorAll(".test");
+console.log(ele);
+
+ 
