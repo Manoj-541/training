416690a9efeba8d39004145e938b7ee1a4f87d1e
Manoj-541 <152593959+Manoj-541@users.noreply.github.com>
2025-01-28 13:02:10 +0530

    Add files via upload

--- train/dom.js
+++ train/dom.js
@@ -39,22 +39,80 @@
 //  document.body.appendChild(image);
 // !
 
-let form = document.querySelector("form");
-let username = document.getElementById("uName");
-let mail = document.getElementById("uMail");
-let pass = document.getElementById("uPass");
+// let form = document.querySelector("form");
+// let username = document.getElementById("uName");
+// let mail = document.getElementById("uMail");
+// let pass = document.getElementById("uPass");
 
-form.addEventListener("submit",(event)=>{
-    event.preventDefault();
+// form.addEventListener("submit",(event)=>{
+//     event.preventDefault();
 
-    let uname =  username.value;
-    let umail = mail.value;
-    let upass = pass.value;
-    let userDetails={
-        uname, umail , upass
-    }
-    console.log(userDetails);
-    localStorage.setItem("userData", JSON.stringify(userDetails))
+//     let uname =  username.value;
+//     let umail = mail.value;
+//     let upass = pass.value;
+//     let userDetails={
+//         uname, umail , upass
+//     }
+//     console.log(userDetails);
+//     localStorage.setItem("userData", JSON.stringify(userDetails))
+    
     
 
+// })
+// !
+let form = document.querySelector("form");
+
+let username = document.getElementById("uName");
+let password = document.getElementById("uPass");
+let check = document.getElementById("check");
+ let show = document.getElementById("show");
+ let gender = document.getElementsByName("gender");
+//  ! hiding  and showing the password values
+check.addEventListener("click", event =>{
+    //console.log(event.target.checked);
+    if(event.target.checked == true){
+        password.setAttribute("type","text");
+        show.innerText = "Hide password";
+    }else{
+        password.setAttribute("type","password");
+        show.innerText = "Show password";
+    }
+
 })
+
+
+// // console.log(gender);    
+
+form.addEventListener("submit" , event=>{
+    event.preventDefault();
+    let un = username.value;
+    let up = password.value;
+    let gen = "";
+
+
+    for(let i=0;i<=gender.length-1;i++){
+        // console.log(gender[i].value);
+        // console.log(gender[i].checked);
+        if(gender[i].checked == true){
+            // console.log(gender[i].value);
+            gen = gender[i].value;
+
+        }
+
+    }
+
+    // 
+    // 
+    // console.log(un , up , gen);
+
+    let userDetails ={
+        username:un,
+        password:up,
+        gender:gen
+    }
+    console.log(userDetails);
+    sessionStorage.setItem("userData" ,
+        JSON.stringify(userDetails)
+    )
+})
+
